54d806b9e5f18ba8797e4e4a72ec6b9ba65661c8
afrizzal <afrizzal@gmail.com>
2021-03-16 11:38:48 +0700

    listNotes mengirim dataForm ke formNotes melalui emit

--- resources/src/components/formNotes.vue
+++ resources/src/components/formNotes.vue
@@ -3,6 +3,7 @@
 		<form @submit="submitNote">			
 			<div class="menu">
 				<button type="button" @click="submitRemove" class="bg-danger btn btn-delete">Delete</button>
+				<button type="submit" class="bg-success btn">Save</button>'
 				<button type="submit" class="bg-success btn">Save</button>
 			</div>
 
@@ -72,35 +73,35 @@
 
 <style>
 .menu{
-	 background: #f7f7f7;
-	 padding:10px 25px;
-	 margin-bottom: 25px;
-	 text-align: right;
-	 border-bottom: 1px solid #e8e6e6;
+	background: #f7f7f7;
+	padding:10px 25px;
+	margin-bottom: 25px;
+	text-align: right;
+	border-bottom: 1px solid #e8e6e6;
 }
 .btn-delete{ margin-right:10px; }
 .content{
-	 padding: 0px 25px;
+	padding: 0px 25px;
 }
 .text{
-	 display: block;
-	 width: 100%;
-	 padding: 0px;
-	 font-size: 20px;
-	 font-weight: bold;
-	 color: #2c3e50;
-	 border: none;
-	 margin-bottom: 10px;
-	 box-sizing: border-box;
-	 outline: none;
+	display: block;
+	width: 100%;
+	padding: 0px;
+	font-size: 20px;
+	font-weight: bold;
+	color: #2c3e50;
+	border: none;
+	margin-bottom: 10px;
+	box-sizing: border-box;
+	outline: none;
 }
 .textarea{
-	 min-height: 350px;
-	 font-size: 15px;
-	 font-weight: lighter;
-	 line-height: 30px;
+	min-height: 350px;
+	font-size: 15px;
+	font-weight: lighter;
+	line-height: 30px;
 }
 .loader{
-	 vertical-align: middle;
+	vertical-align: middle;
 }
 </style>
