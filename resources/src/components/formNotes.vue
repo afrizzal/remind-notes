704e052e2e691a863f5111fcff97df9dcd50c44e
afrizzal <afrizzal@gmail.com>
2021-03-17 11:20:12 +0700

    Pisah button Save & Update

--- resources/src/components/formNotes.vue
+++ resources/src/components/formNotes.vue
@@ -1,9 +1,10 @@
 <template>
 	<div class="formNotes">
-		<form @submit="submitNote">			
+		<form>			
 			<div class="menu">
 				<button type="button" @click="submitRemove" class="bg-danger btn btn-delete">Delete</button>
-				<button type="submit" class="bg-success btn">Save</button>
+				<button type="button" @click="submitSave" class="bg-success btn" v-if="mode == 'save'">Save</button>
+				<button type="button" @click="submitUpdate" class="bg-success btn" v-if="mode == 'update'">Update</button>
 			</div>
 
 			<div class="content">
@@ -12,6 +13,7 @@
 				<textarea class="text textarea" placeholder="Tuliskan rencana kamu..." v-model="description"></textarea>
 			</div>
 		</form>
+		
 	</div>
 </template>
 
@@ -37,18 +39,16 @@
 			return {
 				id: 0,
 				title: '',
-				description: ''
+				description: '',
+				mode: 'save'
 			}
 		},
 		methods: {
-			submitNote(e){
-				e.preventDefault();
-				if (this.id === 0){
+			submitSave(){
 				this.propSaveNote(this.title, this.description);
-			}
-				else{
+			},
+			submitUpdate(){
 				this.propUpdateNote(this.id, this.title, this.description);
-				}
 			},
 			submitRemove(){
 				this.propRemoveNote(this.id);
@@ -65,6 +65,7 @@
 				this.id = note.id;
 				this.title = note.title;
 				this.description = note.description;
+				this.mode = note.mode;
 			}
 		}
 	}
@@ -72,35 +73,35 @@
 
 <style>
 .menu{
-	 background: #f7f7f7;
-	 padding:10px 25px;
-	 margin-bottom: 25px;
-	 text-align: right;
-	 border-bottom: 1px solid #e8e6e6;
+	background: #f7f7f7;
+	padding:10px 25px;
+	margin-bottom: 25px;
+	text-align: right;
+	border-bottom: 1px solid #e8e6e6;
 }
 .btn-delete{ margin-right:10px; }
 .content{
-	 padding: 0px 25px;
+	padding: 0px 25px;
 }
 .text{
-	 display: block;
-	 width: 100%;
-	 padding: 0px;
-	 font-size: 20px;
-	 font-weight: bold;
-	 color: #2c3e50;
-	 border: none;
-	 margin-bottom: 10px;
-	 box-sizing: border-box;
-	 outline: none;
+	display: block;
+	width: 100%;
+	padding: 0px;
+	font-size: 20px;
+	font-weight: bold;
+	color: #2c3e50;
+	border: none;
+	margin-bottom: 10px;
+	box-sizing: border-box;
+	outline: none;
 }
 .textarea{
-	 min-height: 350px;
-	 font-size: 15px;
-	 font-weight: lighter;
-	 line-height: 30px;
+	min-height: 350px;
+	font-size: 15px;
+	font-weight: lighter;
+	line-height: 30px;
 }
 .loader{
-	 vertical-align: middle;
+	vertical-align: middle;
 }
 </style>
