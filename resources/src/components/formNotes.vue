6843451b2782081677d2a6e77589717b75f4b45f
afrizzal <apratama8@gmail.com>
2021-07-18 22:09:34 +0700

    Add formNotes

--- resources/src/components/formNotes.vue
+++ resources/src/components/formNotes.vue
@@ -124,35 +124,35 @@
 }
 .loader{
 	vertical-align: middle;
-     background: #f7f7f7;
-     padding:10px 25px;
-     margin-bottom: 25px;
-     text-align: right;
-     border-bottom: 1px solid #e8e6e6;
+	 background: #f7f7f7;
+	 padding:10px 25px;
+	 margin-bottom: 25px;
+	 text-align: right;
+	 border-bottom: 1px solid #e8e6e6;
 }
 .btn-delete{ margin-right:10px; }
 .content{
-     padding: 0px 25px;
+	 padding: 0px 25px;
 }
 .text{
-     display: block;
-     width: 100%;
-     padding: 0px;
-     font-size: 20px;
-     font-weight: bold;
-     color: #2c3e50;
-     border: none;
-     margin-bottom: 10px;
-     box-sizing: border-box;
-     outline: none;
+	 display: block;
+	 width: 100%;
+	 padding: 0px;
+	 font-size: 20px;
+	 font-weight: bold;
+	 color: #2c3e50;
+	 border: none;
+	 margin-bottom: 10px;
+	 box-sizing: border-box;
+	 outline: none;
 }
 .textarea{
-     min-height: 350px;
-     font-size: 15px;
-     font-weight: lighter;
-     line-height: 30px;
+	 min-height: 350px;
+	 font-size: 15px;
+	 font-weight: lighter;
+	 line-height: 30px;
 }
 .loader{
-     vertical-align: middle;
+	 vertical-align: middle;
 }
 </style>
